017ed078f0e6790ac56276b68db2fa40c01594f8
Alexander Standke <xanderstrike@gmail.com>
2015-11-11 20:48:49 -0800

    add custom labels and input selecting

--- hotkeys.js
+++ hotkeys.js
@@ -2,9 +2,23 @@ $(document).ready(function() {
   var bindings = [];
   $("a[class^='hk-']").each(function(i) {
     var shortcode = $(this).attr('class').replace('hk-', '')
-    console.log("Binding "+shortcode+" to "+$(".hk-"+shortcode).text());
+    var link = $(".hk-"+shortcode)[0]
+    label = $(link).attr('data-hk-label') || $(".hk-"+shortcode).text();
+
+    console.log("Binding "+shortcode+" to "+label);
     $(document).bind('keydown', shortcode.replace('-', '+'), function() {
-      $(".hk-"+shortcode)[0].click();
+      link.click();
+    });
+  });
+
+  $("input[class^='hk-']").each(function(i) {
+    var shortcode = $(this).attr('class').replace('hk-', '')
+    var input = $(".hk-"+shortcode)[0]
+    label = $(input).attr('data-hk-label') || $(".hk-"+shortcode).attr('placeholder');
+
+    console.log("Binding "+shortcode+" to "+label);
+    $(document).bind('keyup', shortcode.replace('-', '+'), function() {
+      input.focus();
     });
   });
 });
